03719c22da0f4c8ec3364236b1999d00dcb2de6f
carlosCorrea <ccorrea14@cuc.edu.co>
2024-10-29 13:13:13 -0500

    administrar con iconos

--- src/app/componentes/canciones/CancionCrear.tsx
+++ src/app/componentes/canciones/CancionCrear.tsx
@@ -1,9 +1,71 @@
 export const CancionCrear = () => {
-    return (
-      <>
-        Cancion crear <br />
-        Canciones...
-      </>
-    );
-  };
-  
+  return (
+    <>
+      <div className="pt-5 d-flex justify-content-center">
+        <div className="col-md-8">
+          <form className="row g-3 needs-validation">
+            <div className="col-md-6 position-relative">
+              <label htmlFor="titu" className="form-label">
+                Titulo cancion
+              </label>
+              <input
+                type="text"
+                className="form-control border-info"
+                id="titu"
+                name="titu"
+                required
+              />
+            </div>
+            <div className="col-md-6 position-relative">
+              <label htmlFor="cant" className="form-label">
+                cantante
+              </label>
+              <input
+                type="text"
+                className="form-control border-info"
+                id="cant"
+                name="cant"
+                required
+              />
+            </div>
+
+            <div className="col-md-6 position-relative">
+              <label htmlFor="dura" className="form-label">
+                Duracion
+              </label>
+              <input
+                type="text"
+                className="form-control border-info"
+                id="dura"
+                name="dura"
+                required
+              />
+            </div>
+
+            <div className="col-md-6 position-relative">
+              <label htmlFor="gene" className="form-label">
+                Genero
+              </label>
+              <select className="form-select border-info" id="gene" name="gene" required>
+                <option selected disabled value="">
+                  Seleccione el genero
+                </option>
+                <option>salsa</option>
+                <option>vallenato</option>
+                <option>champeta</option>
+                <option>regueton</option>
+                <option>pop</option>
+              </select>
+            </div>
+
+            <div className="col-12">
+              <button className="btn btn-primary" type="submit">
+                Registrar cancion
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
